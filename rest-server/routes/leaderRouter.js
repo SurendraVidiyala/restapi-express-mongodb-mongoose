0ac9395eb7a5b5d85fc8fc36a2a2fcb0d6a53822
surendra_vidiyala <vidiyala.surendra@gmail.com>
2017-06-06 17:12:28 -0500

    updated the routes

--- rest-server/routes/leaderRouter.js
+++ rest-server/routes/leaderRouter.js
@@ -1,46 +1,61 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
 var leaderRouter = express.Router();
-
 leaderRouter.use(bodyParser.json());
-
 leaderRouter.route('/')
-    .all(function (req, res, next) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        next();
+    .get(function(req, res, next) {
+        Dishes.find({}, function(err, dish) {
+            if (err) throw err;
+            res.json(dish);
+        });
     })
 
-    .get(function (req, res, next) {
-        res.end('Will send all the leaders to you!');
-    })
+.post(function(req, res, next) {
+    Dishes.create(req.body, function(err, dish) {
+        if (err) throw err;
+        console.log('Dish created!');
+        var id = dish._id;
 
-    .post(function (req, res, next) {
-        res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
-    })
-
-    .delete(function (req, res, next) {
-        res.end('Deleting all leaders');
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Added the dish with id: ' + id);
     });
+})
+
+.delete(function(req, res, next) {
+    Dishes.remove({}, function(err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
+});
+
 
 leaderRouter.route('/:leaderId')
-    .all(function (req, res, next) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        next();
+    .get(function(req, res, next) {
+        Dishes.findById(req.params.dishId, function(err, dish) {
+            if (err) throw err;
+            res.json(dish);
+        });
     })
 
-    .get(function (req, res, next) {
-        res.end('Will send details of the leader: ' + req.params.leaderId + ' to you!');
-    })
+.put(function(req, res, next) {
+    Dishes.findByIdAndUpdate(req.params.dishId, {
+        $set: req.body
+    }, {
+        new: true
+    }, function(err, dish) {
+        if (err) throw err;
+        res.json(dish);
+    });
+})
 
-    .put(function (req, res, next) {
-        res.write('Updating the leader: ' + req.params.leaderId + '\n');
-        res.end('Will update the leader: ' + req.body.name +
-            ' with details: ' + req.body.description);
-    })
-
-    .delete(function (req, res, next) {
-        res.end('Deleting leader: ' + req.params.leaderId);
-    })
+.delete(function(req, res, next) {
+    Dishes.findByIdAndRemove(req.params.dishId, function(err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
+});
 
 module.exports = leaderRouter;
--- rest-server/routes/promoRouter.js
+++ rest-server/routes/promoRouter.js
@@ -1,46 +1,60 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
 var promoRouter = express.Router();
-
 promoRouter.use(bodyParser.json());
-
 promoRouter.route('/')
-    .all(function (req, res, next) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        next();
+    .get(function(req, res, next) {
+        Dishes.find({}, function(err, dish) {
+            if (err) throw err;
+            res.json(dish);
+        });
     })
 
-    .get(function (req, res, next) {
-        res.end('Will send all the promotions to you!');
-    })
+.post(function(req, res, next) {
+    Dishes.create(req.body, function(err, dish) {
+        if (err) throw err;
+        console.log('Dish created!');
+        var id = dish._id;
 
-    .post(function (req, res, next) {
-        res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
-    })
-
-    .delete(function (req, res, next) {
-        res.end('Deleting all promotions');
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Added the dish with id: ' + id);
     });
+})
+
+.delete(function(req, res, next) {
+    Dishes.remove({}, function(err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
+});
 
 promoRouter.route('/:promoId')
-    .all(function (req, res, next) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        next();
+    .get(function(req, res, next) {
+        Dishes.findById(req.params.dishId, function(err, dish) {
+            if (err) throw err;
+            res.json(dish);
+        });
     })
 
-    .get(function (req, res, next) {
-        res.end('Will send details of the promotion: ' + req.params.promoId + ' to you!');
-    })
+.put(function(req, res, next) {
+    Dishes.findByIdAndUpdate(req.params.dishId, {
+        $set: req.body
+    }, {
+        new: true
+    }, function(err, dish) {
+        if (err) throw err;
+        res.json(dish);
+    });
+})
 
-    .put(function (req, res, next) {
-        res.write('Updating the promotion: ' + req.params.promoId + '\n');
-        res.end('Will update the promotion: ' + req.body.name +
-            ' with details: ' + req.body.description);
-    })
-
-    .delete(function (req, res, next) {
-        res.end('Deleting promotion: ' + req.params.promoId);
-    })
+.delete(function(req, res, next) {
+    Dishes.findByIdAndRemove(req.params.dishId, function(err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    });
+});
 
 module.exports = promoRouter;
